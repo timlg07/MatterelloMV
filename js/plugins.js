36dcc3cdda923d46dbf8b72e05ec2b3885988bd3
T!M Greller <mail@tim-greller.ml>
2018-04-23 22:19:49 +0200

    [CYCLES] included
    - included cycles plugin

--- js/plugins.js
+++ js/plugins.js
@@ -11,5 +11,6 @@ var $plugins =
 {"name":"_quickTimeEvent","status":true,"description":"","parameters":{}},
 {"name":"_lightEffects","status":true,"description":"","parameters":{}},
 {"name":"_flashback","status":true,"description":"","parameters":{}},
-{"name":"_slotMachine","status":true,"description":"","parameters":{}}
+{"name":"_slotMachine","status":true,"description":"","parameters":{}},
+{"name":"_cycles","status":true,"description":"","parameters":{}}
 ];
