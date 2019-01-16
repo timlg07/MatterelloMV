1d76cdc570323afac55e2d49a7abb2aa637f38cc
T!M Greller <timlg07@gmail.com>
2019-01-16 18:27:18 +0100

    created windows version & new folder structure

---
+++ releases/windows/MatterelloMV/www/js/plugins/_savepoints.js
@@ -0,0 +1,51 @@
+function Savepoints(){
+    alert("error_STATIC_CLASS");
+}
+
+//The names of all Savepoints:
+Savepoints.names = [
+    "Autosave",
+    "Killer",
+    "My family wants me dead?",
+    "Flashback 1",
+    "Investigation",
+    "Flashback 2",
+    "Boss Fight"
+];
+
+Savepoints.getName = function(savefileId){
+    realID = savefileId - 1;
+    return Savepoints.names[realID] || "";
+}
+
+Savepoints.maxSavefiles = function() {
+    return this.names.length + 1;
+}
+
+//SAVE
+Savepoints.doAutosave = function(){
+    try{
+        if( DataManager.saveGameWithoutRescue( 1 )) SoundManager.playSave();
+    }catch(e){
+        alert(e);
+        try{StorageManager.remove(1);}catch(e2){}
+    }
+}
+
+Savepoints.saveGame = function(savefileId) {
+    if(savefileId==1){
+        return false;
+    }//NEW@TIM#SAVE
+    try {
+        return DataManager.saveGameWithoutRescue(savefileId);
+    } catch (e) {
+        alert(e);
+        try {
+            StorageManager.remove(savefileId);
+        }catch(e2){doNothing()}
+        return false;
+    }
+};
+
+//...
+function doNothing(){}
